07beca254cb85c7749f8a316465991040d675ba2
Makoa DeVincent <makoadevincent@gmail.com>
2020-03-26 06:10:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,8 @@ def long_planeteer_calls(long_call)# code an argument here
   long_call.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(food)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  food.detect {|cheese| cheese}
 end
